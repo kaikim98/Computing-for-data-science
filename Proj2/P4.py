bcc2835ab035986d95829d2ecd184d9297364637
Donghyun_98 <kaikim9809@gmail.com>
2021-04-12 01:20:12 +0900

    21.04.12

--- Proj2/P4.py
+++ Proj2/P4.py
@@ -5,8 +5,11 @@ Please see instruction document.
 """
 from linked_list_helper import ListNode
 def P4(head: ListNode) -> ListNode: 
-    
-    return head
-    ##### End of your code #####
-
-
+    first = None
+    while head:
+        last = head.next
+        head.next = first
+        first = head
+        head = last
+            
+    return first
